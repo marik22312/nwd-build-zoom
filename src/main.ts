0635795ad97cd66c7d1aab423e017243cc28e70f
mark22312 <mark22312@gmail.com>
2022-02-19 15:31:54 +0200

    Added share screen

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ window.onload = () => {
   const shareScreenButton = document.getElementById(
     "share-screen-button"
   ) as HTMLButtonElement;
+  const stopshareButton = document.getElementById(
+    "stop-share-button"
+  ) as HTMLButtonElement;
   const controlsDiv = document.querySelector(".controls");
   const connectCamera = document.getElementById(
     "connect-camera-button"
@@ -16,12 +19,36 @@ window.onload = () => {
   ) as HTMLVideoElement;
   const myPeerIdDisplay = document.getElementById("my-peer-id");
 
+  let activestream: MediaStream;
+
   connectCamera.addEventListener("click", async () => {
-    const userMediaSession = await navigator.mediaDevices.getUserMedia({audio: true, video: true});
-	userVideoDisplay.srcObject = userMediaSession;
+    const userMediaSession = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    });
+    setUserStream(userMediaSession);
   });
 
   shareScreenButton.addEventListener("click", async () => {
-    alert("ShareScreen");
+    const userMediaSession = await navigator.mediaDevices.getDisplayMedia({
+      audio: true,
+      video: true,
+    });
+	setUserStream(userMediaSession);
   });
+
+  const setUserStream = (stream: MediaStream) => {
+	if (activestream) {
+		activestream.getTracks().forEach(track => track.stop());
+	}
+	
+	activestream = stream;
+	userVideoDisplay.srcObject = stream;
+  }
+
+  stopshareButton.addEventListener('click', () => {
+	  if (activestream) {
+		  activestream.getTracks().forEach(track => track.stop())
+	  }
+  })
 };
